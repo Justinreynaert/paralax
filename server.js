11d96321164488b627cba7da76d3ab26fabe4acb
JustinReynaert <Justin.Reynaert@gmail.com>
2017-05-23 10:37:56 +0200

    recruitment

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname)));
 
+app.get('/videos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'videos.html'))
+});
+
 app.get('/recruitment', (req, res) => {
     res.sendFile(path.join(__dirname, 'recruitment.html'))
 });
