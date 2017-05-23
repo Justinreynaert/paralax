2e5b884130b60df552921b09b4ed9770131ea416
JustinReynaert <Justin.Reynaert@gmail.com>
2017-05-23 15:55:23 +0200

    angular frontend

---
+++ angular-src/src/app/components/register/register.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+
+  constructor(
+      private validateService: ValidateService,
+      private flashMessage: FlashMessagesService,
+      private authService: AuthService,
+      private router: Router
+  ) { }
+
+
+
+
+  ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    // required fields
+    if(!this.validateService.validateRegister(user)){
+      this.flashMessage.show('please Fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+
+    }
+
+    //validate email
+    if(!this.validateService.validateEmail(user.email)){
+      this.flashMessage.show('no valid email', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+
+    }
+
+    // Register User
+    this.authService.registerUser(user).subscribe(data => {
+      console.log(data);
+      if(data.success) {
+        this.flashMessage.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessage.show('Something went wrong', {cssClass: 'alert-danger', timeout: 3000});
+        this.router.navigate(['/register']);
+      }
+    });
+
+  };
+
+
+}
